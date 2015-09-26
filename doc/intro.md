cb63e66ba6f99af78ccf55ae8f2262859d79db7f
klapnebojsa <klapnebojsa@gmail.com>
2015-09-27 00:43:07 +0200

    .

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to mire-master
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/mire_master/core_test.clj
@@ -0,0 +1,7 @@
+(ns mire-master.core-test
+  (:require [clojure.test :refer :all]
+            [mire-master.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
